830a30739a7ce9aa6d4151294bea249f49b34fd7
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-01-24 14:12:10 +0100

    Add supoport for generating pdf invoices

--- src/UnicontaRest/Controllers/InvoiceController.cs
+++ src/UnicontaRest/Controllers/InvoiceController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Uniconta.API.DebtorCreditor;
 using Uniconta.API.System;
@@ -15,7 +17,7 @@ namespace UnicontaRest.Controllers
     {
         [HttpPost("Orders/{orderNumber:int}")] // Legacy
         [HttpPost("DebtorOrders/{orderNumber:int}")]
-        public async Task<ActionResult<InvoicePostingResult>> CreateDebtorInvoice(int orderNumber, bool simulate = false)
+        public async Task<ActionResult<InvoicePostingResult>> CreateDebtorInvoice(int orderNumber, bool simulate = false, CompanyLayoutType documentType = CompanyLayoutType.Invoice)
         {
             var crudApi = new CrudAPI(Session, Company);
             var invoiceApi = new InvoiceAPI(Session, Company);
@@ -30,11 +32,27 @@ namespace UnicontaRest.Controllers
 
             var orderLines = await crudApi.Query<DebtorOrderLineClient>(order);
 
-            var invoice = await invoiceApi.PostInvoice(order, orderLines, DateTime.Now, InvoiceNumber: 0 /* Autogenerate */, Simulate: simulate);
+            Task<InvoicePostingResult> invoiceTask;
+
+            if (RequestsPdf())
+            {
+                invoiceTask = invoiceApi.PostInvoicePDF(order, orderLines, DateTime.Now, InvoiceNumber: 0 /* Autogenerate */, simulate, documentType);
+            }
+            else
+            {
+                invoiceTask = invoiceApi.PostInvoice(order, orderLines, DateTime.Now, InvoiceNumber: 0 /* Autogenerate */, simulate);
+            }
+
+            var invoice = await invoiceTask;
 
             if (invoice.Err != ErrorCodes.Succes)
             {
                 return StatusCode(500, invoice.Err);
+            }
+
+            if (invoice.pdf is object)
+            {
+                return File(invoice.pdf, "application/pdf");
             }
 
             return Ok(invoice);
@@ -56,27 +74,50 @@ namespace UnicontaRest.Controllers
 
             var orderLines = await crudApi.Query<CreditorOrderLineClient>(order);
 
-            var invoice = await invoiceApi.PostInvoice(
-                order, orderLines, DateTime.Now,
-                InvoiceNumber: 0 /* Autogenerate */,
-                Simulate: simulate,
-                InvoiceType: null,
-                InvTransType: null,
-                SendEmail: false,
-                ShowInvoice: false,
-                DocumentType: documentType,
-                Emails: null,
-                OnlyToThisEmail: false,
-                GLTransType: null,
-                Documents: null,
-                PostOnlyDelivered: false);
+            Task<InvoicePostingResult> invoiceTask;
+
+            if (RequestsPdf())
+            {
+                invoiceTask = invoiceApi.PostInvoicePDF(order, orderLines, DateTime.Now, InvoiceNumber: 0 /* Autogenerate */, simulate, documentType);
+            }
+            else
+            {
+                invoiceTask = invoiceApi.PostInvoice(
+                    order, orderLines, DateTime.Now,
+                    InvoiceNumber: 0 /* Autogenerate */,
+                    Simulate: simulate,
+                    InvoiceType: null,
+                    InvTransType: null,
+                    SendEmail: false,
+                    ShowInvoice: true,
+                    DocumentType: documentType,
+                    Emails: null,
+                    OnlyToThisEmail: false,
+                    GLTransType: null,
+                    Documents: null,
+                    PostOnlyDelivered: false);
+            }
+
+            var invoice = await invoiceTask;
 
             if (invoice.Err != ErrorCodes.Succes)
             {
                 return StatusCode(500, invoice.Err);
             }
 
+            if (invoice.pdf is object)
+            {
+                return File(invoice.pdf, "application/pdf");
+            }
+
             return Ok(invoice);
+        }
+
+        private bool RequestsPdf()
+        {
+            var acceptHeader = Request.GetTypedHeaders().Accept;
+
+            return acceptHeader?.Any(x => x.MediaType.Equals("application/pdf", StringComparison.OrdinalIgnoreCase)) ?? false;
         }
     }
 }
